29c33ced6a6b4385d1dd6a700e6ea5a6c7ceffa6
znxbcv2121 <sangkim2017@u.northwestern.edu>
2016-11-19 10:51:08 -0600

    Initial Commit

---
+++ app/src/main/java/intodarkness/android/intodarkness/MainActivity.java
@@ -0,0 +1,74 @@
+package intodarkness.android.intodarkness;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+
+import co.tanvas.haptics.service.app.*;
+import co.tanvas.haptics.service.adapter.*;
+import co.tanvas.haptics.service.model.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private HapticView mHapticView;
+    private HapticTexture mHapticTexture;
+    private HapticMaterial mHapticMaterial;
+    private HapticSprite mHapticSprite;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initHaptics();
+    }
+
+    public void initHaptics() {
+        try {
+
+            // Get the service adapter
+            HapticServiceAdapter serviceAdapter = HapticApplication.getHapticServiceAdapter();
+
+            // Create a haptic view and activate it
+            mHapticView = HapticView.create(serviceAdapter);
+            mHapticView.activate();
+
+            // Set the orientation of the haptic view
+            Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+            int rotation = display.getRotation();
+            HapticView.Orientation orientation = HapticView.getOrientationFromAndroidDisplayRotation(rotation);
+            mHapticView.setOrientation(orientation);
+
+            // Retrieve texture data from the bitmap
+            Bitmap hapticBitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.noise_texture);
+            byte[] textureData =
+                    HapticTexture.createTextureDataFromBitmap(hapticBitmap);
+
+            // Create a haptic texture with the retrieved texture data
+            mHapticTexture = HapticTexture.create(serviceAdapter);
+            int textureDataWidth = hapticBitmap.getRowBytes() / 4; // 4 channels, i.e., ARGB
+            int textureDataHeight = hapticBitmap.getHeight();
+            mHapticTexture.setSize(textureDataWidth, textureDataHeight);
+            mHapticTexture.setData(textureData);
+
+            // Create a haptic material with the created haptic texture
+            mHapticMaterial = HapticMaterial.create(serviceAdapter);
+            mHapticMaterial.setTexture(0, mHapticTexture);
+
+            // Create a haptic sprite with the haptic material
+            mHapticSprite = HapticSprite.create(serviceAdapter);
+            mHapticSprite.setMaterial(mHapticMaterial);
+
+            // Add the haptic sprite to the haptic view
+            mHapticView.addSprite(mHapticSprite);
+        } catch (Exception e) {
+            Log.e(null, e.toString());
+        }
+    }
+
+}
